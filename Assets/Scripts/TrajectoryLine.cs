b909cddc37599218fc485e363e4a6a07f6183905
Rohan5210 <rohan5210work@gmail.com>
2025-09-02 18:45:02 +0530

    successfuly inmplemented powerup 1

--- Assets/Scripts/TrajectoryLine.cs
+++ Assets/Scripts/TrajectoryLine.cs
@@ -1,29 +1,55 @@
+using System;
 using UnityEngine;
 
 public class TrajectoryLine : MonoBehaviour
 {
-    [Header("Trajectory Line Settings")]
-    [SerializeField] private LineRenderer lineRenderer;
-    [SerializeField] private int segmentCount = 10;
-    private Vector3[] linePositions;
+    #region Members
+    LineRenderer trajectoryLine;
+    [SerializeField, Tooltip("The marker will show where the projectile will hit")]
+    int maxPoints = 50;
+    [SerializeField, Range(0.01f, 0.5f), Tooltip("The time increment used to calculate the trajectory")]
+    float increment = 0.025f;
+    [SerializeField, Range(1.05f, 2f), Tooltip("The raycast overlap between points in the trajectory, this is a multiplier of the length between points. 2 = twice as long")]
+    float rayOverlap = 1.1f;
+    public static TrajectoryLine Instance { get; private set; }
+    #endregion
 
-    void Start()
+    private void Awake()
     {
-        lineRenderer = GetComponent<LineRenderer>();
-        CreateLine();
+        Instance = this;
+        trajectoryLine = GetComponent<LineRenderer>();
     }
 
-    private void CreateLine()
+    public void Trajectory(Vector3 velocity, Vector3 position)
     {
-        lineRenderer.positionCount = segmentCount;
-        linePositions = new Vector3[segmentCount];
+        Vector3 nextPosition;
+        float overlap;
+        UpdateLineRender(maxPoints, (0, position));
+
+        for (int i = 1; i < maxPoints; i++)
+        {
+            velocity = CalculateNewVelocity(velocity, increment);
+            nextPosition = position + velocity * increment;
+            overlap = Vector3.Distance(position, nextPosition) * rayOverlap;
+            if (Physics.Raycast(position, velocity.normalized, out RaycastHit hit, overlap))
+            {
+                UpdateLineRender(i, (i - 1, hit.point));
+                break;
+            }
+            position = nextPosition;
+            UpdateLineRender(maxPoints, (i, position)); //Unneccesary to set count here, but not harmful
+        }
+    }
+      private void UpdateLineRender(int count, (int point, Vector3 pos) pointPos)
+    {
+        trajectoryLine.positionCount = count;
+        trajectoryLine.SetPosition(pointPos.point, pointPos.pos);
     }
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 CalculateNewVelocity(Vector3 velocity, float increment)
     {
-        // Update the line positions if needed
-        CreateLine();
+        velocity += Physics.gravity * increment;
+        velocity *= Mathf.Clamp01(1f - increment);
+        return velocity;
     }
-    
 }
